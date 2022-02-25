085abef35f228de6f9d4c95dbb30ebd414a1b543
goesrafa <rafa.goes.2012@gmail.com>
2022-02-25 14:33:42 -0300

    Inicio

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace dotnet_____poo
+{
+        class Program
+        {
+                static void  Main(string[] args)
+                {
+                    string nome = "Ashe";
+                    int lvl = 30;
+                    string tipoHeroi = "Arqueira";
+
+                    Console.Write(nome + "  "  + lvl +  "  " +  tipoHeroi);
+                }            
+        }
+}
---
+++ src/Entities/Ashe.cs
@@ -0,0 +1,7 @@
+namespace Abstração_de_um_Jogo_RPG.src.Entities
+{
+    public class Ashe
+    {
+        
+    }
+}
